4d4e2ab239755726ad6acd80aebbf11fa2d34ad9
erictran0x <40257345+erictran0x@users.noreply.github.com>
2025-11-23 13:59:10 -0800

    renaming things

--- terraform/modules/backend/s3.tf
+++ terraform/modules/backend/s3.tf
@@ -2,31 +2,31 @@
 # INPUT BUCKET CONFIGURATION #
 ##############################
 
-resource "aws_s3_bucket" "input_bucket" {
-	bucket = "itg-stamdb-input"
+resource "aws_s3_bucket" "input" {
+  bucket = "itg-stamdb-input"
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "input_lifecycle" {
-	bucket = aws_s3_bucket.input_bucket.id
+  bucket = aws_s3_bucket.input.id
 
-	rule {
-		id 			= "ExpireAllInOneDay"
-		status 	= "Enabled"
+  rule {
+    id     = "ExpireAllInOneDay"
+    status = "Enabled"
 
-		expiration {
-			days = 1
-		}
-	}
+    expiration {
+      days = 1
+    }
+  }
 }
 
 resource "aws_s3_bucket_notification" "input_notifications" {
-  bucket = aws_s3_bucket.input_bucket.id
+  bucket = aws_s3_bucket.input.id
 
   dynamic "queue" {
     for_each = toset([".sm", ".ssc"])
     content {
-      id = "add-to-queue-on-${queue.value}-file-upload"
-      queue_arn     = aws_sqs_queue.input_queue.arn
+      id            = "add-to-queue-on-${queue.value}-file-upload"
+      queue_arn     = aws_sqs_queue.input.arn
       events        = ["s3:ObjectCreated:*"]
       filter_suffix = queue.value
     }
@@ -37,12 +37,12 @@ resource "aws_s3_bucket_notification" "input_notifications" {
 # OUTPUT BUCKET CONFIGURATION #
 ###############################
 
-resource "aws_s3_bucket" "output_bucket" {
+resource "aws_s3_bucket" "output" {
   bucket = "itg-stamdb-output"
 }
 
 resource "aws_s3_bucket_public_access_block" "output_public_access" {
-  bucket = aws_s3_bucket.output_bucket.id
+  bucket = aws_s3_bucket.output.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -51,7 +51,7 @@ resource "aws_s3_bucket_public_access_block" "output_public_access" {
 }
 
 resource "aws_s3_bucket_versioning" "output_versioning" {
-  bucket = aws_s3_bucket.output_bucket.id
+  bucket = aws_s3_bucket.output.id
 
   versioning_configuration {
     status = "Enabled"
--- terraform/modules/backend/sqs.tf
+++ terraform/modules/backend/sqs.tf
@@ -1,48 +1,54 @@
-resource "aws_sqs_queue" "input_queue" {
-	name = "itg-stamdb-input-queue"
+resource "aws_sqs_queue" "input" {
+  name = "itg-stamdb-input-queue"
 
-	visibility_timeout_seconds = 30
+  visibility_timeout_seconds = 30
 }
 
-resource "aws_sqs_queue" "failed_queue" {
-	name = "itg-stamdb-failed-queue"
+resource "aws_sqs_queue" "failed" {
+  name = "itg-stamdb-failed-queue"
 }
 
-resource "aws_sqs_queue_policy" "input_queue_policy" {
-  queue_url = aws_sqs_queue.input_queue.id
+resource "aws_sqs_queue_policy" "input_policy" {
+  queue_url = aws_sqs_queue.input.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect    = "Allow"
+      Effect = "Allow"
       Principal = {
         Service = "s3.amazonaws.com"
       }
-      Action    = "sqs:SendMessage"
-      Resource  = aws_sqs_queue.input_queue.arn
+      Action   = "sqs:SendMessage"
+      Resource = aws_sqs_queue.input.arn
       Condition = {
         ArnLike = {
-          "aws:SourceArn" = aws_s3_bucket.input_bucket.arn
+          "aws:SourceArn" = aws_s3_bucket.input.arn
         }
       }
     }]
   })
 }
 
-resource "aws_sqs_queue_redrive_policy" "failed_queue_policy" {
-	queue_url = aws_sqs_queue.input_queue.id
-
-	redrive_policy = jsonencode({
-		maxReceiveCount     = 1
-		deadLetterTargetArn = aws_sqs_queue.failed_queue.arn
-	})
+resource "aws_lambda_event_source_mapping" "input_trigger" {
+  event_source_arn = aws_sqs_queue.input.arn
+  function_name    = aws_lambda_function.functions["itg_stamdb_processor"].arn
+  batch_size       = 10
 }
 
-resource "aws_sqs_queue_redrive_allow_policy" "failed_queue_allow_policy" {
-	queue_url = aws_sqs_queue.failed_queue.id
+resource "aws_sqs_queue_redrive_policy" "failed_policy" {
+  queue_url = aws_sqs_queue.input.id
 
-	redrive_allow_policy = jsonencode({
-		redrivePermission = "byQueue"
-		sourceQueueArns  	= [aws_sqs_queue.input_queue.arn]
-	})
+  redrive_policy = jsonencode({
+    maxReceiveCount     = 1
+    deadLetterTargetArn = aws_sqs_queue.failed.arn
+  })
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "failed_allow_policy" {
+  queue_url = aws_sqs_queue.failed.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue"
+    sourceQueueArns   = [aws_sqs_queue.input.arn]
+  })
 }
