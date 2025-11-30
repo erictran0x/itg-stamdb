c1bab9c4e0e16880e7414093ab96950065c5d327
erictran0x <40257345+erictran0x@users.noreply.github.com>
2025-11-29 23:43:00 -0800

    write human-readable names for each arrow

--- stamina-breakdown-parser/patterns.py
+++ stamina-breakdown-parser/patterns.py
@@ -5,10 +5,10 @@ from typing import TypedDict, Literal
 from breakdown import Stream
 
 class Arrow(Enum):
-    L = 0b_1000
-    D = 0b_0100
-    U = 0b_0010
-    R = 0b_0001
+    L = 0b_1000, 'left'
+    D = 0b_0100, 'down'
+    U = 0b_0010, 'up'
+    R = 0b_0001, 'right'
 
 type ArrowKey = Literal[*[x.name for x in Arrow]]
 
@@ -16,7 +16,7 @@ type ArrowKey = Literal[*[x.name for x in Arrow]]
 def convert_arrows_to_bitmask(*arrows):
     bitmasks = list(map(
         lambda x: reduce(
-            lambda a, b: a << 4 | getattr(Arrow, b).value,
+            lambda a, b: a << 4 | getattr(Arrow, b).value[0],
             map(lambda y: y, [c for c in x]),
             0
         ),
@@ -69,20 +69,23 @@ def count_patterns(streams: list[Stream], pattern_type: PatternType) -> list[int
         return result
     return list(map(helper, streams))
 
-def count_anchors(streams: list[Stream]) -> dict[ArrowKey, int]:
+def count_anchors(streams: list[Stream]) -> list[dict[ArrowKey, int]]:
     def helper(note_data: list[int], arrow: Arrow):
         # need every tripling of every other note
         triplings = zip(note_data, note_data[2:], note_data[4:])
+        bitmask = arrow.value[0]
         return sum([
             1 for [a, b, c] in triplings
-            if (a & arrow.value) > 0 and (b & arrow.value) > 0 and (c & arrow.value) > 0
+            if (a & bitmask) > 0 and (b & bitmask) > 0 and (c & bitmask) > 0
         ])
-    result = {}
-    for stream in streams:
+    def helper2(stream: Stream) -> dict[ArrowKey, int]:
+        result = {}
         note_data = stream['note_data']
         for arrow in Arrow:
-            if arrow.name not in result:
-                result[arrow.name] = 0
-            result[arrow.name] += helper(note_data, arrow)
-    return result
+            arrow_name = arrow.value[1].upper()
+            if arrow_name not in result:
+                result[arrow_name] = 0
+            result[arrow_name] += helper(note_data, arrow)
+        return result
+    return list(map(helper2, streams))
 
