6865128b299ebadb52c618f57e46d10761b102cd
erictran0x <40257345+erictran0x@users.noreply.github.com>
2025-12-03 23:43:24 -0800

    add clickable tooltip

--- frontend/src/routes/about.tsx
+++ frontend/src/routes/about.tsx
@@ -1,16 +1,33 @@
-import { Box, Button, Heading } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import { Tooltip } from '@/components/ui/tooltip'
 import { createFileRoute } from '@tanstack/react-router'
+import { useState, type ReactNode } from 'react';
 
 export const Route = createFileRoute('/about')({
   component: RouteComponent,
 })
 
+function ClickableTooltip({ content, children }: { content: string, children?: ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Tooltip
+      closeOnClick={false} content={content}
+      openDelay={0} open={isOpen} onOpenChange={(e) => setIsOpen(e.open)}>
+      <a
+        onClick={() => setIsOpen(!isOpen)}
+        className='dotted-underline'>
+        {children}
+      </a>
+    </Tooltip>
+  );
+}
+
 function RouteComponent() {
   return (
     <Box w={{ base: '100%', md: '80%' }} mx="auto" textAlign="center" py={8}
       fontSize={{ base: 'md', md: 'lg', lg: 'xl' }}
-      lineHeight={{ base: '1.6', md: '1.9' }}
+      lineHeight={{ base: '1.6' }}
     >
       <Heading as="h1" size="3xl" mb={4}>
         Motivation
@@ -18,21 +35,21 @@ function RouteComponent() {
       <p>
         As a Stepmania/ITG stamina stepartist, one of the few challenges they face is being
         able to&nbsp;
-        <Tooltip content="assign a number to">
-          <a className='dotted-underline'>rate</a>
-        </Tooltip>
+        <ClickableTooltip content="assign a number to">
+          rate
+        </ClickableTooltip>
         &nbsp;their charts correctly, or at least in a fair manner.
         Currently, the best way to do so is to cross-reference other charts of&nbsp;
-        <Tooltip content="basically how many measures of 16th notes before a break and how long the breaks are">
-          <a className='dotted-underline'>similar stream breakdowns</a>
-        </Tooltip>
+        <ClickableTooltip content="basically how many measures of 16th notes before a break and how long the breaks are">
+          similar stream breakdowns
+        </ClickableTooltip>
         &nbsp;and rate their charts based on the other ones.
         However, these breakdowns aren't in one place, making this task a bit annoying.
         <br />
         The purpose of this site is to put the breakdowns of all&nbsp;
-        <Tooltip content="stuff in East Coast Stamina and Stamina RPG">
-          <a className='dotted-underline'>known tournament stamina charts</a>
-        </Tooltip>
+        <ClickableTooltip content="stuff in East Coast Stamina and Stamina RPG">
+          known tournament stamina charts
+        </ClickableTooltip>
         &nbsp;into one place, making it easier to cross-reference other charts.
       </p>
       <br />
