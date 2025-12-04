90dbe0f7fef6948799d7916da52e2f09ae97c430
erictran0x <40257345+erictran0x@users.noreply.github.com>
2025-12-03 18:40:46 -0800

    re-index charts for ngram tokenizer

--- terraform/modules/backend/opensearch.tf
+++ terraform/modules/backend/opensearch.tf
@@ -1,6 +1,25 @@
 resource "opensearch_index" "charts" {
   name = "charts"
 
+  analysis_tokenizer = jsonencode({
+    my_edge_ngram_tokenizer = {
+      type        = "edge_ngram"
+      min_gram    = 2
+      max_gram    = 25
+      token_chars = ["letter", "digit", "symbol"]
+    }
+  })
+
+  analysis_analyzer = jsonencode({
+    my_custom_analyzer = {
+      tokenizer = "my_edge_ngram_tokenizer"
+      filter    = ["lowercase"]
+    }
+  })
+
+  number_of_shards   = "1"
+  number_of_replicas = "1"
+
   mappings = jsonencode({
     properties = {
       chart_hash = {
@@ -8,6 +27,12 @@ resource "opensearch_index" "charts" {
       }
       title = {
         type = "text"
+        fields = {
+          autocomplete = {
+            type     = "text"
+            analyzer = "my_custom_analyzer"
+          }
+        }
       }
     }
   })
